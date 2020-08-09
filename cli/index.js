061347e1ec33799addc060801e8dc574e18d6047
RONDINELE SANTOS DE MORAIS <60003107@rondinellimorais.local>
2020-08-09 10:06:00 -0300

    melhorias

--- cli/index.js
+++ cli/index.js
@@ -1,18 +1,21 @@
 const semver = require("semver");
 
-const range = ">=0.2.1 <1.0.0 || >=1.2.3";
-const versions = ["0.2.1", "1.0.0", "1.2.3", '1.2.0'].sort(semver.rcompare);
+const patched_versions = ">=0.2.1 <1.0.0 || >=1.2.3";
+const currentVersion = '1.2.0';
+const versions = ["0.2.1", "1.0.0", "1.2.3", currentVersion].sort(semver.rcompare);
+
 console.log(versions);
+console.log(`current version: ${currentVersion}`);
 
 const maxSatisfyingVersion = semver.maxSatisfying(
   versions,
-  range
+  patched_versions
 );
 console.log(`max version: ${maxSatisfyingVersion}`);
 
 const minSatisfyingVersion = semver.minSatisfying(
   versions,
-  range
+  patched_versions
 );
 
 console.log(`min version: ${minSatisfyingVersion}`);
